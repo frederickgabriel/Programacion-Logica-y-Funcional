748a8284b82d0822acf5457146c464cbcc83c219
frederickgabriel <frederickaguilar317@gmail.com>
2026-03-18 19:31:45 -0500

    Subi mi tarea del dia de hoy

---
+++ Programacion funcional/Ejercicios_2.1/01_basico/02_types.py
@@ -0,0 +1,34 @@
+#por frederick gabriel aguilar puc
+from os import system
+if system('clear') != 0: system('cls')
+
+print(int)
+print(type(10))
+print(type(0))
+print(type(-5))
+print(type(202018281281208819812892))
+
+print(float)
+print(type(3.14))
+print(type(1.0))
+print(type(1e3))
+
+print("complex")
+print(type(1+2))
+
+print(str)
+print(type("hola"))
+print(type(""))
+print(type("123"))
+print(type("""
+    multilinea
+"""
+))
+
+print("bool")
+print(type(True))
+print(type(False))
+print(type(1<2))
+
+print("NoneType")
+print(type(None))
